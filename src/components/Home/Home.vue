c771a8cfcca45d971634603b0bf119b6fa65b2f7
ludan <1274161203@qq.com>
2019-08-02 18:51:53 +0800

    init

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+  <div></div>
 </template>
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
+/* eslint-disable no-undef */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from '@/App'
 import router from './router'
+import 'normalize.css'
+import 'border.css'
 
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
