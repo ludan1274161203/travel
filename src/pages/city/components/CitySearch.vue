efb0c4f5be91e73d861a264bd3d91cab5287ceaa
ludan <1274161203@qq.com>
2019-08-13 09:59:59 +0800

    city-search

---
+++ src/pages/city/components/CitySearch.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="scss" scoped>
+@import '~styles/_variable.scss';
+.search {
+  height: 0.72rem;
+  background-color: $bgcolor;
+  padding: 0.1rem;
+  .search-input {
+    height: 0.62rem;
+    line-height: 0.62rem;
+    text-align: center;
+    border-radius: 0.06rem;
+    width: 100%;
+    color: #666;
+    box-sizing: border-box;
+    padding: 0rem 0.1rem;
+  }
+}
+</style>
